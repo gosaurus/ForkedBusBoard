21c1d90eec2a4b18b6e60269ca71045656103044
Stephanie Chiang <stephanie.chiang@softwire.com>
2025-02-01 12:40:42 +0000

    first commit, to be overriden from the public BusBoard project

--- testFetch.js
+++ testFetch.js
@@ -1,21 +1,55 @@
-import fetch from 'node-fetch';
+//Forked version@https://github.com/gosaurus/ForkedBusBoard.git
 
+import fetch from 'node-fetch';
+const testArray = [
+  { Destination: 'Highgate Village', Route: '214', TimeToStation: 295 },
+  {
+    Destination: 'Highgate Village',
+    Route: '214',
+    TimeToStation: 1235
+  },
+  { Destination: 'Highgate Village', Route: '214', TimeToStation: 910 },
+  {
+    Destination: 'Highgate Village',
+    Route: '214',
+    TimeToStation: 1663
+  },
+  {
+    Destination: 'Parliament Hill Fields',
+    Route: '88',
+    TimeToStation: 578
+  },
+  {
+    Destination: 'Parliament Hill Fields',
+    Route: '88',
+    TimeToStation: 275
+  },
+  {
+    Destination: 'Parliament Hill Fields',
+    Route: '88',
+    TimeToStation: 388
+  },
+  {
+    Destination: 'Parliament Hill Fields',
+    Route: '88',
+    TimeToStation: 1004
+  }
+]
 //Fetch TFL Stop-Point API. Note at this stage we are taking URL as it is with Stop Code. We will change it later to user prompt.
-async function fetchAPI()
-{
-const response = await fetch("https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals");
-let data = await response.json();
-await extractData(data);
-}
+// async function fetchAPI() {
+//     const response = await fetch("https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals");
+//     let data = await response.json();
+//     return data;
+// }
 
 // Extract required data from API (Destination,Route Number,TimetoStation(seconds)); Returns list of objects 
-async function extractData(data) {
+function extractData(data) {
     const extractInfo = data.map(busDetails=>({
-        Destination:busDetails.destinationName, 
-        Route:busDetails.lineName,
-        TimeToStation:busDetails.timeToStation
+        Destination: busDetails.destinationName, 
+        Route: busDetails.lineName,
+        TimeToStation: busDetails.timeToStation
     }));
-    // console.log(extractInfo);
+    console.log(extractInfo);
     return extractInfo
 }
 
@@ -24,6 +58,9 @@ function arrivalTime(extractInfo) {
     let sortedBuses = extractInfo.sort((a,b) = a.busDetails.timeToStation - b.busDetails.timeToStation);
 }
 
-// Calling FetchAPI
-
-fetchAPI();
+// Main function
+async function main() {
+    // const apiResponseData = await fetchAPI(); // returns API data
+    // const extractedInfo = extractData(apiResponseData); // returns extracted data
+    
+}
